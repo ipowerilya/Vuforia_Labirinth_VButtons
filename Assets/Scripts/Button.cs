b6340b1342373141b95e8a30880d979e81fdd281
ipowerilya <idestinyilya@gmail.com>
2020-05-07 17:25:19 +0300

    Commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Button : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject Vbutton;
+    public Animator anm;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        anm.Play("Rotate");
+        Debug.Log("Button Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        anm.Play("None");
+        Debug.Log("Button Relised");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vbutton = GameObject.Find("VirtualButton");
+        Vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        anm.GetComponent<Animator>();
+    }
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject Spawn;
+    public Rigidbody rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < plane.transform.position.y - 2)
+        {
+            transform.position = Spawn.transform.position;
+            rb.velocity = new Vector3(0f, 0f, 0f);
+
+        }
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
